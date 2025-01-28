e10ed3fe306702cdb837490b6211d4c73e9b0d66
GuilhermeVoltalia <g.rodrigues-ext@voltalia.com>
2025-01-28 20:36:24 -0300

    refactor: remover middleware de autenticação e adicionar exibição do ID no perfil do admin

--- src/app/(private)/admin/perfil/[id]/page.tsx
+++ src/app/(private)/admin/perfil/[id]/page.tsx
@@ -2,11 +2,18 @@
 
 import PrivateLayout from "@/shared/layout/private/Private";
 import { TopBar } from "@/shared/components";
+import { useRouter } from "next/router";
 
-export default function Page({ params }: { params: { id: string } }) {
+export default function Page() {
+  const router = useRouter();
+
+  const { id } = router.query;
+
   return (
     <PrivateLayout>
       <TopBar />
+
+      {id}
     </PrivateLayout>
   );
 }
